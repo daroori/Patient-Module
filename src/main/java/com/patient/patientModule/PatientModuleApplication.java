48847ac4fd24e28e96328618f8bc4675a3aa661a
daroo <darooritaruntej@gmail.com>
2024-10-30 13:32:54 -0500

    Initial Commit

---
+++ src/main/java/com/patient/patientModule/PatientModuleApplication.java
@@ -0,0 +1,13 @@
+package com.patient.patientModule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientModuleApplication.class, args);
+	}
+
+}
